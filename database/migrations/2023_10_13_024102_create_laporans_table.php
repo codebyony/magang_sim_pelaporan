48e9866b2b06c3118826661045aa4b5fbef99ef9
onynovianti <onynovianti26@gmail.com>
2023-10-13 09:48:10 +0700

    update migration

---
+++ database/migrations/2023_10_13_024102_create_laporans_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('laporans', function (Blueprint $table) {
+            $table->id();
+            $table->string('NIP');
+            $table->string('nama_karyawan');
+            $table->string('divisi');
+            $table->string('judul');
+            $table->text('isi');
+            $table->string('kategori');
+            $table->string('lampiran');
+            $table->string('nomor_wa');
+            $table->string('sifat');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('laporans');
+    }
+};
